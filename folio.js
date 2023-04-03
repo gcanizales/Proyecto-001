97f92271569fd6067a6330885d70c55d52aa77f8
Gabriel Canizales Castillo <125100949+gcanizales@users.noreply.github.com>
2023-04-03 00:29:48 -0600

    Update folio.js

--- folio.js
+++ folio.js
@@ -1,1 +1,13 @@
+<script>
+  // Obtén la referencia al campo de texto donde mostrarás el número de folio
+const folio = document.getElementById('folio');
 
+// Obtén la unidad seleccionada en el formulario
+const unidad = document.getElementById('unidad').value;
+
+// Genera un número de folio automático con las iniciales de la unidad
+const numeroFolio = unidad.substr(0, 2) + Math.floor(Math.random() * 1000);
+
+// Actualiza el valor del campo de texto con el número de folio generado
+folio.value = numeroFolio;
+</script>
